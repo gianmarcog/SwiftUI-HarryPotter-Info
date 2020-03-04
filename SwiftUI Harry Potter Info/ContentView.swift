1a69b80139f0c4e382b509f67543dd5279b7b0fb
gianmarcog <marco.giretti@web.de>
2020-03-04 10:16:13 +0100

    Initial Commit

---
+++ SwiftUI Harry Potter Info/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Harry Potter Info
+//
+//  Created by Gianmarco Giretti on 04.03.20.
+//  Copyright © 2020 Gianmarco Giretti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
