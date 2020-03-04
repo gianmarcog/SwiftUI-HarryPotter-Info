b779aaf32c23ad617753b71d7256f4b6a9580c1b
gianmarcog <marco.giretti@web.de>
2020-03-04 11:26:27 +0100

    added classes

---
+++ SwiftUI Harry Potter Info/HP-Info.swift
@@ -0,0 +1,9 @@
+//
+//  HP-Info.swift
+//  SwiftUI Harry Potter Info
+//
+//  Created by Gianmarco Giretti on 04.03.20.
+//  Copyright © 2020 Gianmarco Giretti. All rights reserved.
+//
+
+import Foundation
