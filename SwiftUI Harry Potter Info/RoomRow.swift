cf04780908ab37d98f80b67fc478215be2f7651f
gianmarcog <marco.giretti@web.de>
2020-03-05 11:03:50 +0100

    Added Rile

---
+++ SwiftUI Harry Potter Info/RoomRow.swift
@@ -0,0 +1,39 @@
+ //
+//  RoomRow.swift
+//  SwiftUI Harry Potter Info
+//
+//  Created by Gianmarco Giretti on 05.03.20.
+//  Copyright © 2020 Gianmarco Giretti. All rights reserved.
+//
+
+import SwiftUI
+
+struct RoomRow: View {
+    
+    var categoryName: String
+    var rooms: [Room]
+    
+    var body: some View {
+        VStack(alignment: HorizontalAlignment.leading) {
+            
+            Text(self.categoryName)
+                .font(.title)
+            
+            ScrollView(showsIndicators: false){
+                HStack(alignment: .top) {
+                    ForEach(self.rooms, id: \.name) { room in
+                        RoomItem(room: room)
+                            .frame(width: 300)
+                            .padding(.trailing, 30)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct RoomRow_Previews: PreviewProvider {
+    static var previews: some View {
+        RoomRow(categoryName: "HOT ROOMS", rooms: roomData)
+    }
+}
